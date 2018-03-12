7b46a9d2435e5df09e3a98e99ca9bae16abb84c7
James Long <jc500@duke.edu>
2018-03-12 15:21:29 -0400

    add element span calculations

--- @fwObj/make_xdc.m
+++ @fwObj/make_xdc.m
@@ -3,6 +3,7 @@ function obj = make_xdc(obj, tx_params)
 %  Function to create transducer related fields
 %
 %  Calling:
+%           tx_params = struct('name','c52v','event','plane','theta',0)
 %           obj.make_xdc(tx_params)
 %
 %  Parameters:
@@ -19,8 +20,8 @@ obj.xdc = xdc_lib(tx_params.name);
 obj.xdc.inmap = zeros(size(obj.field_maps.cmap));
 obj.xdc.outmap = zeros(size(obj.field_maps.cmap));
 
-%%% Check if grid spacing if adequate for modeling %%%%%%%%%%%%
-if obj.grid_vars.dY > obj.xdc.pitch, error('Grid spacing is too small.'); end
+%%% Check if grid spacing if adequate for modeling %%%%%%%%%%%%%%%%%%%%%%%%
+if obj.grid_vars.dY > obj.xdc.pitch, error('Grid spacing is too large.'); end
 
 if strcmp(obj.xdc.type, 'curvilinear')
     dtheta = atand(obj.xdc.pitch/obj.xdc.r);
@@ -35,7 +36,7 @@ if strcmp(obj.xdc.type, 'curvilinear')
     xlabel('y (mm)'); ylabel('z (mm)'); title(obj.xdc.name);
     set(gcf, 'visible', 'off');
     
-    %%% Calculate indices of xdc in space %%%%%%%%%%%%%%%%%%%%%
+    %%% Calculate indices of xdc in space %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     [~,idx_y] = min(abs(obj.grid_vars.y_axis-y'));
     idx_y = find(idx_y ~= 1 & idx_y ~= length(y));
     [~,idx_z] = min(abs(obj.grid_vars.z_axis'-z));
@@ -48,7 +49,7 @@ if strcmp(obj.xdc.type, 'curvilinear')
     obj.xdc.incoords = mapToCoords(obj.xdc.inmap);
     obj.xdc.outcoords = mapToCoords(obj.xdc.outmap);
     
-    %%% Calculate delays and generate icmat %%%%%%%%%%%%%%%%%%%
+    %%% Calculate delays and generate icmat %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     tfield = calc_tfield(obj, tx_params);
     dur = 2; % exponential drop-off of envelope;
     icvec1 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
@@ -60,6 +61,11 @@ if strcmp(obj.xdc.type, 'curvilinear')
     icvec4 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
         .*sin((tfield-3*obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
     obj.xdc.icmat = [icvec1; icvec2; icvec3; icvec4];
+    
+    %%% Calculate element index spans %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    e_size = round(size(idx_y,2)/obj.xdc.n);
+    e_start = round(linspace(1, size(idx_y,2)-e_size+1, obj.xdc.n));
+    obj.xdc.e_ind = [e_start' (e_start+e_size)'];
     
 elseif strcmp(obj.xdc.type, 'linear')
     
