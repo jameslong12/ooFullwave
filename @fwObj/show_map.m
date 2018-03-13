20259032513c57b99cfd4644a04d7a77f125a711
James Long <jc500@duke.edu>
2018-03-13 09:58:02 -0400

    add clim

--- @fwObj/show_map.m
+++ @fwObj/show_map.m
@@ -15,17 +15,21 @@ if ~exist('map_name','var'), map_name = 'cmap'; end
 switch map_name
     case {'cmap', 'c'}
         map = obj.field_maps.cmap';
+        lim = [0.75*obj.input_vars.c0 1.25*obj.input_vars.c0];
     case {'rhomap', 'rho'}
         map = obj.field_maps.rhomap';
+        lim = [0.75*obj.input_vars.rho 1.25*obj.input_vars.rho];
     case {'attenmap', 'atten'}
-        map = obj.field_maps.attenmap';
+        map = obj.field_maps.attenmap';\
+        lim = [0 obj.input_vars.atten];
     case {'boveramap', 'bovera'}
         map = obj.field_maps.boveramap';
+        lim = [0 obj.input_vars.boveramap];
     otherwise
         error('Invalid map name.')
 end
 
-imagesc(obj.grid_vars.y_axis, obj.grid_vars.z_axis, map)
+imagesc(obj.grid_vars.y_axis, obj.grid_vars.z_axis, map, lim)
 xlabel('y (mm)'); ylabel('z (mm)'); title(map_name);
 axis image
 colormap jet
