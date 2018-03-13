e59a10501531e40e950cb5c00d8fd46d1a0211a4
James Long <jc500@duke.edu>
2018-03-13 16:02:56 -0400

    make linear case

--- @fwObj/make_xdc.m
+++ @fwObj/make_xdc.m
@@ -61,21 +61,17 @@ if strcmp(obj.xdc.type, 'curvilinear')
     obj.xdc.e_ind = [e_start' (e_start+e_size)'];
     
 elseif strcmp(obj.xdc.type, 'linear')
-    dtheta = atand(obj.xdc.pitch/obj.xdc.r);
-    span = 2*obj.xdc.r*sind((obj.xdc.n-1)*dtheta/2);
+    span = obj.xdc.n*obj.xdc.pitch;
     y = -span/2:obj.grid_vars.dY:span/2;
-    z = sqrt(obj.xdc.r^2.-y.^2); z = z-min(z);
     
     %%% Calculate indices of xdc in space %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     [~,idx_y] = min(abs(obj.grid_vars.y_axis-y'));
     idx_y = find(idx_y ~= 1 & idx_y ~= length(y));
-    [~,idx_z] = min(abs(obj.grid_vars.z_axis'-z));
-    idx_z = idx_z(2:end-1);
     for i = 1:length(idx_y)
-        obj.xdc.inmap(idx_y(i),idx_z(i):idx_z(i)+3) = ones(4,1);
-        obj.xdc.outmap(idx_y(i),idx_z(i)+3) = 1;
+        obj.xdc.inmap(idx_y(i),1:3) = ones(3,1);
+        obj.xdc.outmap(idx_y(i),3) = 1;
     end
-    obj.xdc.idx_y = idx_y; obj.xdc.idx_z = idx_z;
+    obj.xdc.idx_y = idx_y;
     obj.xdc.incoords = mapToCoords(obj.xdc.inmap);
     obj.xdc.outcoords = mapToCoords(obj.xdc.outmap);
     
@@ -88,9 +84,7 @@ elseif strcmp(obj.xdc.type, 'linear')
         .*sin((tfield-obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
     icvec3 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
         .*sin((tfield-2*obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
-    icvec4 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
-        .*sin((tfield-3*obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
-    obj.xdc.icmat = [icvec1; icvec2; icvec3; icvec4];
+    obj.xdc.icmat = [icvec1; icvec2; icvec3;];
     
     %%% Calculate element index spans %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     e_size = round(size(idx_y,2)/obj.xdc.n);
