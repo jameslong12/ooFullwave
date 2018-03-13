daa6afea37d5701c5ab09a5c8658d3d85c55c331
James Long <jc500@duke.edu>
2018-03-13 14:25:03 -0400

    add field_flag

--- @fwObj/do_sim.m
+++ @fwObj/do_sim.m
@@ -1,12 +1,23 @@
-function rf = do_sim(obj)
+function out = do_sim(obj, field_flag)
 
 %  Method to call Fullwave executable and perform simulation
 %
 %  Calling:
-%           rf = obj.do_sim()
+%           out = obj.do_sim(field_flag)
+%
+%  Parameters: 
+%           field_flag          - Flag to indicate entire field output
 %
 %  James Long 03/10/2018
 
+%%% Use field flag to get pressure across entire map %%%%%%%%%%%%%%%%%%%%%%
+if ~exist('field_flag','var'), field_flag = 0; end
+if field_flag
+    obj.xdc.outmap = ones(size(obj.xdc.outmap));
+    obj.xdc.outcoords = mapToCoords(obj.xdc.outmap);
+end
+
+%%% Launch FullWave executable %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if ispc
     launchTotalFullWaveRebuild2(obj.input_vars.c0, obj.input_vars.omega0,...
         obj.input_vars.wY, obj.input_vars.wZ, obj.input_vars.td,...
@@ -32,13 +43,17 @@ end
 ncoordsout=size(obj.xdc.outcoords,1);
 nRun=sizeOfFile('genout.dat')/4/ncoordsout;
 genout = readGenoutSlice(['genout.dat'],0:nRun-1,size(obj.xdc.outcoords,1));
-for idx = 1:size(obj.xdc.outcoords)
-    genout_re(:,obj.xdc.outcoords(idx,1)-min(obj.xdc.outcoords(:,1))+1) = double(genout(:,idx));
-end
-
-%%% Average across output to reconstruct element traces %%%%%%%%%%%%%%%%%%%
-for idx = 1:obj.xdc.n
-    rf(:,idx) = mean(genout_re(:,obj.xdc.e_ind(idx,:)),2);
+if field_flag
+    out = reshape(genout, size(genout,1), size(obj.xdc.outmap,2), size(obj.xdc.outmap,1));
+else
+    for idx = 1:size(obj.xdc.outcoords)
+        genout_re(:,obj.xdc.outcoords(idx,1)-min(obj.xdc.outcoords(:,1))+1) = double(genout(:,idx));
+    end
+    
+    %%% Average across output to reconstruct element traces %%%%%%%%%%%%%%%
+    for idx = 1:obj.xdc.n
+        out(:,idx) = mean(genout_re(:,obj.xdc.e_ind(idx,:)),2);
+    end
 end
 
 end
--- @fwObj/make_xdc.m
+++ @fwObj/make_xdc.m
@@ -61,6 +61,41 @@ if strcmp(obj.xdc.type, 'curvilinear')
     obj.xdc.e_ind = [e_start' (e_start+e_size)'];
     
 elseif strcmp(obj.xdc.type, 'linear')
+    dtheta = atand(obj.xdc.pitch/obj.xdc.r);
+    span = 2*obj.xdc.r*sind((obj.xdc.n-1)*dtheta/2);
+    y = -span/2:obj.grid_vars.dY:span/2;
+    z = sqrt(obj.xdc.r^2.-y.^2); z = z-min(z);
+    
+    %%% Calculate indices of xdc in space %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    [~,idx_y] = min(abs(obj.grid_vars.y_axis-y'));
+    idx_y = find(idx_y ~= 1 & idx_y ~= length(y));
+    [~,idx_z] = min(abs(obj.grid_vars.z_axis'-z));
+    idx_z = idx_z(2:end-1);
+    for i = 1:length(idx_y)
+        obj.xdc.inmap(idx_y(i),idx_z(i):idx_z(i)+3) = ones(4,1);
+        obj.xdc.outmap(idx_y(i),idx_z(i)+3) = 1;
+    end
+    obj.xdc.idx_y = idx_y; obj.xdc.idx_z = idx_z;
+    obj.xdc.incoords = mapToCoords(obj.xdc.inmap);
+    obj.xdc.outcoords = mapToCoords(obj.xdc.outmap);
+    
+    %%% Calculate delays and generate icmat %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    tfield = calc_tfield(obj, tx_params);
+    dur = 2; % exponential drop-off of envelope;
+    icvec1 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
+        .*sin(tfield*obj.input_vars.omega0)*obj.input_vars.p0;
+    icvec2 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
+        .*sin((tfield-obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
+    icvec3 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
+        .*sin((tfield-2*obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
+    icvec4 = exp(-(1.05*tfield*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur))...
+        .*sin((tfield-3*obj.grid_vars.dY/obj.input_vars.c0)*obj.input_vars.omega0)*obj.input_vars.p0;
+    obj.xdc.icmat = [icvec1; icvec2; icvec3; icvec4];
+    
+    %%% Calculate element index spans %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    e_size = round(size(idx_y,2)/obj.xdc.n);
+    e_start = round(linspace(1, size(idx_y,2)-e_size+1, obj.xdc.n));
+    obj.xdc.e_ind = [e_start' (e_start+e_size)'];
     
 else
     error('Unsupported transducer type.')
