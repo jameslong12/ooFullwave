5a6adfa102efd7a8981bea1aadcc5e4a10cb8c72
James Long <jc500@duke.edu>
2018-11-28 16:53:05 -0500

    fix index averaging to match e_ind information

--- @fwObj/average_icmat.m
+++ @fwObj/average_icmat.m
@@ -1,18 +1,10 @@
 function icmat_avg = average_icmat(obj,icmat)
 
 icmat_avg = zeros(size(icmat));
+ind_avg = round(mean(obj.xdc.e_ind,2));
 
-%%% Reindex elements based on icmat, not inmap
-ist = obj.xdc.e_ind(1);
 for i = 1:size(obj.xdc.e_ind,1)
-    ind_new(i,:) = [ist ist+obj.xdc.e_ind(i,end)-obj.xdc.e_ind(i,1)];
-    ist = ist+obj.xdc.e_ind(i,end)-obj.xdc.e_ind(i,1)+1;
-end
-ind_avg = round(mean(ind_new,2));
-%inds = (obj.xdc.e_ind(:,1):obj.xdc.e_ind(:,end))-min(obj.xdc.e_ind(:))+1;
-
-for i = 1:size(ind_new,1)
-    inds = ind_new(i,1):ind_new(i,end);
+    inds = obj.xdc.e_ind(i,1):obj.xdc.e_ind(i,end);
     icmat_avg(inds,:) = repmat(icmat(ind_avg(i),:),[length(inds) 1]);
 end
 
