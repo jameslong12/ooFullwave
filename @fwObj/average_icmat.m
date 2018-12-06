4e8930b3b9de871fc5f0540d37066947a1b11a67
James Long <jc500@duke.edu>
2018-12-05 21:08:38 -0500

    remove zeros at start of transmit

--- @fwObj/average_icmat.m
+++ @fwObj/average_icmat.m
@@ -1,4 +1,4 @@
-function icmat_avg = average_icmat(obj,icmat)
+function icmat_corr = average_icmat(obj,icmat)
 
 icmat_avg = zeros(size(icmat));
 ind_avg = round(mean(obj.xdc.e_ind,2));
@@ -7,5 +7,11 @@ for i = 1:size(obj.xdc.e_ind,1)
     inds = obj.xdc.e_ind(i,1):obj.xdc.e_ind(i,end);
     icmat_avg(inds,:) = repmat(icmat(ind_avg(i),:),[length(inds) 1]);
 end
+
+icmat_corr = zeros(size(icmat_avg));
+mask = icmat_avg~=0;
+[~,mi] = max(mask,[],2); mii = min(mi(mi~=1));
+
+icmat_corr(:,1:size(icmat_corr,2)-mii+1) = icmat_avg(:,mii:end);
 
 end
