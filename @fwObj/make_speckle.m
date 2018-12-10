21248279ba4b152aab966017ba78546ad9a28804
James Long <jc500@duke.edu>
2018-12-10 14:20:03 -0500

    add inputParser format

--- @fwObj/make_speckle.m
+++ @fwObj/make_speckle.m
@@ -1,4 +1,4 @@
-function obj = make_speckle(obj, sc_params)
+function obj = make_speckle(obj, varargin)
 
 %  Function to create transducer related fields
 %
@@ -17,16 +17,20 @@ function obj = make_speckle(obj, sc_params)
 %
 %  Code inherited from Nick Bottenus, function compiled by James Long, 03/10/2018
 
-%%% Extract fields from sc_params and set defaults %%%%%%%%%%%%%%%%%%%%%%%%
-if exist('sc_params','var')
-    assignments = extract_struct(sc_params);
-    for i = 1:length(assignments)
-        eval(assignments{i});
-    end
+
+
+%%% Use inputParser to set optional parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+p = inputParser;
+addOptional(p,'nlesion',0)
+addOptional(p,'csr',0.05)
+addOptional(p,'nscat',25)
+
+p.parse(varargin{:})
+var_struct = p.Results;
+assignments = extract_struct(var_struct);
+for i = 1:length(assignments)
+    eval(assignments{i})
 end
-if ~exist('n_scat','var'), n_scat = 25; end
-if ~exist('csr','var'), csr = 0.05; end
-if ~exist('n_lesion','var'), n_lesion = 0; end
 
 %%% Calculate resolution cell using input variables %%%%%%%%%%%%%%%%%%%%%%%
 res_cell = rescell2d(obj.input_vars.c0, obj.input_vars.omega0, obj.input_vars.wZ/2,...
@@ -34,17 +38,17 @@ res_cell = rescell2d(obj.input_vars.c0, obj.input_vars.omega0, obj.input_vars.wZ
     obj.grid_vars.dZ);
 
 %%% Randomly place scatterers according to density %%%%%%%%%%%%%%%%%%%%%%%%
-scat_density = n_scat/res_cell;
-cscatmap = rand(obj.grid_vars.nY, obj.grid_vars.nZ);
-cscatmap(cscatmap > scat_density) = 0;
+scat_density = nscat/res_cell;
+cscatmap = rand(obj.grid_vars.nY, obj.grid_vars.nZ)-0.5;
+cscatmap(abs(cscatmap) > scat_density) = 0;
 cscatmap = cscatmap/scat_density;
 
 %%% Create lesions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if n_lesion
-    if size(c_lesion,1) ~= n_lesion || size(r_lesion,1) ~= n_lesion
+if nlesion
+    if size(c_lesion,1) ~= nlesion || size(r_lesion,1) ~= nlesion
         error('r_lesion and c_lesion must be the same length as n_lesion.')
     end
-    for idx = 1:n_lesion
+    for idx = 1:nlesion
         [z_int, y_int] = meshgrid(obj.grid_vars.z_axis, obj.grid_vars.y_axis);
         lesion_mask = (y_int-c_lesion(idx,1)).^2+(z_int-c_lesion(idx,2)).^2 < r_lesion(idx).^2;
         cscatmap(lesion_mask) = 0;
@@ -52,6 +56,6 @@ if n_lesion
 end
 
 %%% Superimpose original cmap and cscatmap %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-obj.field_maps.cmap = obj.field_maps.cmapi-cscatmap*csr*obj.input_vars.c0;
+obj.field_maps.cmap = obj.field_maps.cmap+cscatmap*csr.*obj.field_maps.cmap;
 
 end
