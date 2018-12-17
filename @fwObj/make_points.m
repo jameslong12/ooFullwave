5d2d2b9c85f78f461bbec21d6496c50324f5594b
James Long <jc500@duke.edu>
2018-12-17 11:39:55 -0500

    add method for fixed or adapt, add error messages

--- @fwObj/make_points.m
+++ @fwObj/make_points.m
@@ -13,6 +13,7 @@ function obj = make_points(obj, varargin)
 p = inputParser;
 addOptional(p,'pos',[0 obj.input_vars.wZ/2])    % Laterally centered, half depth
 addOptional(p,'zd',0.5)                         % 50% acoustic impedance
+addOptional(p,'method','fixed')                 % Use set c0
 
 p.parse(varargin{:})
 var_struct = p.Results;
@@ -21,15 +22,21 @@ for i = 1:length(assignments)
     eval(assignments{i})
 end
 
-if length(zd) == 1; zd = zd*ones(size(pos,2)); end
-if length(zd) ~= size(pos,2); error('Size of zd and pos must match.'); end
+if length(zd) == 1; zd = zd*ones(size(pos,1),1); end
+if length(zd) ~= size(pos,1); error('Size of "zd" and "pos" must match.'); end
 
 %%% Place points in map %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for idx = 1:size(pos,2)
+for idx = 1:size(pos,1)
     if abs(pos(idx,1)) > obj.input_vars.wY/2; error('Specified lateral position outside field range.'); end
     if pos(idx,2) > obj.input_vars.wZ || pos(idx,2) < 0; error('Specified axial position outside field range.'); end
     [~,iy] = min(abs(obj.grid_vars.y_axis-pos(idx,1)));
     [~,iz] = min(abs(obj.grid_vars.z_axis-pos(idx,2)));
-    obj.field_maps.cmap(iy-1:iy+1,iz-1:iz+1) = zd(idx)*obj.field_maps(iy,iz);
+    if strcmp(method,'fixed')
+        obj.field_maps.cmap(iy-1:iy+1,iz-1:iz+1) = zd(idx)*obj.input_vars.c0;
+    elseif strcmp(method,'adapt')
+        obj.field_maps.cmap(iy-1:iy+1,iz-1:iz+1) = zd(idx)*obj.field_maps.cmap(iy,iz);
+    else
+        error('Unrecognized method. Use "fixed" or "adapt".')
+    end
 end
 
