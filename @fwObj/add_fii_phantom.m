97eefe5443a11dc5b71f33a3e383fa9e10b9d12a
James Long <jc500@duke.edu>
2018-12-17 12:28:15 -0500

    update info

--- @fwObj/add_fii_phantom.m
+++ @fwObj/add_fii_phantom.m
@@ -1,17 +1,20 @@
-function obj = add_fii_phantom(obj, phtm_file, el_lim, csr, symmetry, fnum)
+function obj = add_fii_phantom(obj, phtm_file, el_lim, csr, fnum)
 
 %  Function to add scatterers to maps
 %
 %  Calling:
-%           obj.make_speckle('nscat',15,'csr',0.05,'nC',0)
+%           obj.add_fii_phantom('phtm_file.mat', 1.5-3, 0.05, 2)
 %
-%  Optional parameters:
-%           nscat:      Scatterers per resolution cell (15)
-%           csr:        Scatterer impedance contrast (0.05)
-%           nC:         Number of cysts (0)
-%           rC:         Vector of cyst radii (m), length equal to nC
-%           cC:         Matrix of cyst center locations in [y,z] (m), length equal to nC
-%           zC:         Vector of cyst impedance contrast, length equal to nC
+%  Parameters:
+%           phtm_file:      Path to phantom file. File must be .mat
+%                           containing "phantom" structure with fields
+%                           "position" and "amplitude" (output from Mark
+%                           Palmeri's ultratrack tools)
+%           el_lim:         Elevational limit [m] (default: 1.5e-3)
+%           csr:            Standard deviation of scatterer impedance
+%                           contrast (default: 0.05)
+%           fnum:           (Optional) Transmit F-number used to calculate
+%                           scatterers per resolution cell
 %
 %  James Long, 12/17/2018
 
@@ -19,9 +22,8 @@ load(phtm_file)
 if ~exist('phantom','var'); error('Variable "phantom" not found in file.'); end
 if size(phantom,2) ~= 3; error('Could not find 3 dimensions to positions.'); end
 
-if nargin < 4; symmetry = 1; end
 if nargin < 3; csr = 0.05; end
-if nargin < 2; el_lim = 1.5e3; warning('Using default elevation limit. Check for adequate scatterer density.'); end
+if nargin < 2; el_lim = 1.5e-3; warning('Using default elevation limit. Check for adequate scatterer density.'); end
 
 %%% Restrict positions in all dimensions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ind = abs(phantom.position(:,2)) < el_lim; % elevation
@@ -29,7 +31,7 @@ py = phantom.position(:,1); py = py(ind);
 pz = phantom.position(:,3); pz = pz(ind);
 amp = phantom.amplitude; amp = amp(ind);
 
-ind = abs(phantom.position(:,1)) < obj.input_vars.wY; % lateral
+ind = abs(phantom.position(:,1)) < obj.input_vars.wY/2; % lateral
 py = phantom.position(:,1); py = py(ind);
 pz = phantom.position(:,3); pz = pz(ind);
 amp = phantom.amplitude; amp = amp(ind);
@@ -39,20 +41,19 @@ py = phantom.position(:,1); py = py(ind);
 pz = phantom.position(:,3); pz = pz(ind);
 amp = phantom.amplitude; amp = amp(ind);
 
+pz = [pz; pz];
+py = [py; -py];
+amp = [amp; amp];
+
 %%% If F-number known, calculate scatterers per resolution cell %%%%%%%%%%%
-scat_density = length(py)/(abs(max(py)*max(pz)));
-ry = obj.input_vars.lambda*fnum; rz = obj.input_vars.ncycles*obj.input_vars.lambda/2;
-rescell = ry*rz; nscat_cell = scat_density*rescell;
-fprintf('   Scatterers per resolution cell = %1.1f',nscat_cell)
-
-%%% Optional code to visualize %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%scatter(py,pz,0.5,'.r'); axis image; title(sprintf('n_{scat per cell} = %1.1f',nscat_cell))
-
-%%% If symmetrical, duplicate phantom %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if symmetry
-    pz = [pz; pz];
-    py = [py; -py];
-    amp = [amp; amp];
+if exist('fnum','var')
+    scat_density = length(py)/(abs(max(py)*max(pz)));
+    ry = obj.input_vars.lambda*fnum; rz = obj.input_vars.ncycles*obj.input_vars.lambda/2;
+    rescell = ry*rz; nscat_cell = scat_density*rescell;
+    fprintf('   Scatterers per resolution cell = %1.1f',nscat_cell)
+    
+    %%% Optional code to visualize %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %scatter(py,pz,0.5,'.r'); axis image; title(sprintf('n_{scat per cell} = %1.1f',nscat_cell))
 end
 
 %%% Add scatterers using impedance flow %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
