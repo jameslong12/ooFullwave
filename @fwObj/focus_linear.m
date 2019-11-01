0f7f40ca06e4e6ba47e4e641941e3706b9990c6c
James Long <jc500@duke.edu>
2019-11-01 15:06:21 -0400

    convert pulse to impulse and excitation

--- @fwObj/focus_linear.m
+++ @fwObj/focus_linear.m
@@ -1,4 +1,4 @@
-function obj = focus_linear(obj, focus)
+function obj = focus_linear(obj, focus, fc, fbw, excitation, bwr, tpe)
 
 %  Function to create transmit related fields. Must call obj.make_xdc()
 %  prior to use.
@@ -8,6 +8,11 @@ function obj = focus_linear(obj, focus)
 %
 %  Parameters:
 %           focus           - Focal point in [y z] (m)
+%           fc              - Center frequency of transducer (Hz) [obj.input_vars.f0]
+%           fbw             - Fractional bandwidth of transducer [0.8]
+%           excitation      - Vector of excitation in time
+%           bwr             - Fractional bandwidth reference level [-6 dB]
+%           tpe             - Cutoff for trailing pulse envelope [-40 dB]
 %
 %  Returns:
 %           obj.xdc.inmap   - Input map for initial conditions
@@ -21,30 +26,51 @@ function obj = focus_linear(obj, focus)
 %
 %  James Long 12/06/2018
 
+if ~exist('fc','var')||isempty(fc), fc = obj.input_vars.f0; end
+if ~exist('fbw','var')||isempty(fbw), fbw = 0.8; end
+if ~exist('bwr','var')||isempty(bwr), bwr=-6; end
+if ~exist('tpe','var')||isempty(tpe), tpe=-40; end
+
 %%% Initialize inmap and incoords %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 obj.xdc.inmap(:,1:3) = 1;
 obj.xdc.incoords = mapToCoords(obj.xdc.inmap);
 
-%%% Calculate delays and generate icmat %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-dur = 2;
+%%% Calculate impulse response and transmitted pulse %%%%%%%%%%%%%%%%%%%%%%
 fy = (focus(1)-obj.grid_vars.y_axis(1))/obj.grid_vars.dY+1;
 fz = (focus(2)-obj.grid_vars.z_axis(1))/obj.grid_vars.dZ+1;
-t = (0:obj.grid_vars.nT-1)/obj.grid_vars.nT*obj.input_vars.td-obj.input_vars.ncycles/obj.input_vars.omega0*2*pi;
+
+tc = gauspuls('cutoff',fc,fbw,bwr,tpe);
+fs=1/obj.grid_vars.dT;
+tv = -tc:1/fs:tc;
+impulse_response = gauspuls(tv,fc,fbw);
+obj.xdc.impulse_t = tv;
+obj.xdc.impulse = impulse_response;
+
+if ~exist('excitation','var')||isempty(excitation)
+    excitation = sin(2*pi*obj.input_vars.f0*(0:1/fs:obj.input_vars.ncycles/obj.input_vars.f0));
+end
+pulse = conv(impulse_response,excitation);
+pulse = pulse/max(abs(pulse));
+obj.xdc.pulse_t = (0:length(pulse)-1)/fs;
+obj.xdc.pulse = pulse;
+
 
 %%% Call focus_transmit to focus transmit beam %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 coord_row = 1:size(obj.xdc.incoords,1)/3;
-icvec = exp(-(1.05*t*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur)).*sin(t*obj.input_vars.omega0)*obj.input_vars.p0;
+t = (0:obj.grid_vars.nT-1)/obj.grid_vars.nT*obj.input_vars.td-obj.input_vars.ncycles/obj.input_vars.omega0*2*pi;
+icvec = zeros(size(obj.grid_vars.t_axis));
+icvec(1:length(pulse)) = pulse*obj.input_vars.p0;
 icmat = average_icmat(obj,focus_transmit(obj,fy,fz,icvec,obj.xdc.incoords(coord_row,:)));
-t=t-obj.grid_vars.dT/obj.input_vars.cfl;
 
 coord_row = size(obj.xdc.incoords,1)/3+1:2*size(obj.xdc.incoords,1)/3;
-icvec = exp(-(1.05*t*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur)).*sin(t*obj.input_vars.omega0)*obj.input_vars.p0;
+tnew=t-obj.grid_vars.dT/obj.input_vars.cfl;
+icvec = interp1(t,icvec,tnew,[],0);
 icmat_add = average_icmat(obj,focus_transmit(obj,fy,fz,icvec,obj.xdc.incoords(coord_row,:)));
 icmat = [icmat; icmat_add];
-t=t-obj.grid_vars.dT/obj.input_vars.cfl;
 
 coord_row = 2*size(obj.xdc.incoords,1)/3+1:size(obj.xdc.incoords,1);
-icvec = exp(-(1.05*t*obj.input_vars.omega0/(obj.input_vars.ncycles*pi)).^(2*dur)).*sin(t*obj.input_vars.omega0)*obj.input_vars.p0;
+tneww=tnew-obj.grid_vars.dT/obj.input_vars.cfl;
+icvec = interp1(tnew,icvec,tneww,[],0);
 icmat_add = average_icmat(obj,focus_transmit(obj,fy,fz,icvec,obj.xdc.incoords(coord_row,:)));
 obj.xdc.icmat = [icmat; icmat_add];
 
