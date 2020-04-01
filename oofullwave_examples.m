b41014fb1683c9de0de514e57a705c6c2fc67350
James Long <jc500@duke.edu>
2020-04-01 13:56:56 -0400

    add arbitrary waveform example

--- oofullwave_examples.m
+++ oofullwave_examples.m
@@ -1,15 +1,15 @@
-%  ooFullwave, v2.0.0
+%  ooFullwave, v2.2.0
 %
 %  Examples using fwObj to setup and run Fullwave simulations for focused,
 %  plane wave, and diverging transmits on linear arrays. Includes examples
 %  with abdominal walls and adding scatterers.
 %
-%  James Long 12/10/2018
+%  James Long 04/01/2020
 %  ***Fullwave written by Gianmarco Pinton***
 
 %% Setup fwObj for varying transmit cases %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear
-close; figure('pos',[-1900 100 1200 600],'color','w')
+close; figure('pos',[100 100 1200 600],'color','w')
 msz = 100;
 
 %%% Create fwObj %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -61,9 +61,76 @@ subplot(236)
 scatter(sim.xdc.on_elements,sim.xdc.delays*1e6,msz,'.b'); axis tight
 xlabel('Element number'); ylabel('Time (us)'); title('Diverging delays')
 
+%% Add arbitrary waveform %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear
+close; figure('pos',[100 100 1200 600],'color','w')
+msz = 100;
+
+%%% Create fwObj %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+c0 = 1540;                                  % Homogeneous speed of sound
+f0 = 2e6;                                   % Transmit center frequency (Hz)
+wZ = 8e-2;                                  % Axial extent (m)
+wY = 6e-2;                                  % Lateral extent (m)
+td =(wZ+1e-2)/c0;                           % Time duration of simulation (s)
+sim = fwObj('c0',c0,'f0',f0,'wY',wY,'wZ',wZ,'td',td);
+
+%%% Specify transducer and transmit parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sim.xdc.type = 'linear';                    % Curvilinear or linear
+sim.xdc.pitch = 0.000412;                   % Center-to-center element spacing
+sim.xdc.kerf = 3.25e-5;                     % Interelement spacing
+sim.xdc.n = 64;                             % Number of elements
+sim.make_xdc();                             % Call make_xdc to set up transducer
+
+%%% 2-cycle pulse %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+focus = [0 0.03];                           % Focal point in [y z] (m)
+sim.focus_linear(focus);                    % Call focus_linear to calculate icmat
+subplot(231)
+linewidth = 2;
+plot(sim.xdc.excitation_t,sim.xdc.excitation,'linewidth',linewidth)
+xlabel('Time (us)'); title('2-cycle excitation'); axis tight
+
+subplot(234)
+plot(sim.xdc.pulse_t,sim.xdc.pulse,'linewidth',linewidth)
+xlabel('Time (us)'); title('2-cycle pulse'); axis tight
+
+%%% 10-cycle pulse %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ncycles = 10;
+sim = fwObj('c0',c0,'f0',f0,'wY',wY,'wZ',wZ,'td',td,'ncycles',ncycles);
+sim.xdc.type = 'linear';                    % Curvilinear or linear
+sim.xdc.pitch = 0.000412;                   % Center-to-center element spacing
+sim.xdc.kerf = 3.25e-5;                     % Interelement spacing
+sim.xdc.n = 64;                             % Number of elements
+sim.make_xdc();                             % Call make_xdc to set up transducer
+focus = [0 0.03];                           % Focal point in [y z] (m)
+sim.focus_linear(focus);                    % Call focus_linear to calculate icmat
+subplot(232)
+linewidth = 2;
+plot(sim.xdc.excitation_t,sim.xdc.excitation,'linewidth',linewidth)
+xlabel('Time (us)'); title('10-cycle excitation'); axis tight
+
+subplot(235)
+plot(sim.xdc.pulse_t,sim.xdc.pulse,'linewidth',linewidth)
+xlabel('Time (us)'); title('10-cycle pulse'); axis tight
+
+%%% Chirp %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+flow = 1e6; fhigh = 3e6;
+t = 0:sim.grid_vars.dT:6e-6; 
+k = (pi/max(t))*(fhigh-flow);
+phi = k*t.^2;
+excitation = sin(2*pi*flow*t+phi);
+sim.focus_linear(focus,[],[],excitation); 
+subplot(233)
+linewidth = 2;
+plot(sim.xdc.excitation_t,sim.xdc.excitation,'linewidth',linewidth)
+xlabel('Time (us)'); title('Chirp excitation'); axis tight
+
+subplot(236)
+plot(sim.xdc.pulse_t,sim.xdc.pulse,'linewidth',linewidth)
+xlabel('Time (us)'); title('Chirp pulse'); axis tight
+
 %% Setup fwObj to make changes to maps %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear
-close; figure('pos',[-1900 100 1400 500],'color','w')
+close; figure('pos',[100 100 1400 500],'color','w')
 cax = [1440 1640];
 
 %%% Create fwObj %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
