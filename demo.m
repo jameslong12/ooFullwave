55281a9795cdc90b06de7e63c28516fe802d7b70
James Long <jc500@duke.edu>
2020-04-01 15:58:33 -0400

    add visualization

--- demo.m
+++ demo.m
@@ -42,3 +42,25 @@ fprintf('   Channel data generated in %1.2f seconds \n',toc(t))
 t = tic;
 p = double(sim.do_sim(1));
 fprintf('   Field pressure data generated in %1.2f seconds \n',toc(t))
+
+%% 8. Visualize %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close
+maxp = max(p(:).^2);
+for i = 1:5:size(p,1)
+    imagesc(sim.grid_vars.y_axis*1e2,sim.grid_vars.z_axis*1e2,squeeze(p(i,:,:))'.^2/maxp,[0 1])
+    title(sprintf('t = %1.2f us',sim.grid_vars.t_axis(i)*1e6))
+    colormap jet
+    axis image
+    xlabel('Lateral (cm)')
+    ylabel('Axial (cm)')
+    drawnow
+end
+
+close
+maxp = squeeze(max(p.^2,[],1))';
+imagesc(sim.grid_vars.y_axis*1e2,sim.grid_vars.z_axis*1e2,maxp)
+colormap jet
+axis image
+xlabel('Lateral (cm)')
+ylabel('Axial (cm)')
+title('Maximum intensity')
