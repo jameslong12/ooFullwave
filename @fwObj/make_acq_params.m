394b93d903ae2742fb01fcae9a5ae98192c02386
James Long <jc500@duke.edu>
2020-05-08 10:21:36 -0400

    update acq_params for curvilinear

--- @fwObj/make_acq_params.m
+++ @fwObj/make_acq_params.m
@@ -12,27 +12,30 @@
 %
 %  Nick Bottenus, 11/27/2018
 
-acq_params=[];
-acq_params.fs=1/obj.grid_vars.dT;                           % Sampling frequency
-acq_params.f0=obj.input_vars.f0;                            % Center frequency
-acq_params.c=obj.input_vars.c0;                             % Speed of sound
-acq_params.t0=obj.xdc.t0/obj.grid_vars.dT;                  % Time 0 of firing
-
+acq_params = [];
+acq_params.fs = 1/obj.grid_vars.dT;
+acq_params.f0 = obj.input_vars.f0;
+acq_params.c = obj.input_vars.c0;
+acq_params.t0 = -length(obj.xdc.pulse)/2;
 if obj.input_vars.v == 2
     acq_params.samples=length(obj.grid_vars.t_axis)-1;
 else
     acq_params.samples=length(obj.grid_vars.t_axis);
 end
-acq_params.rx_pos=obj.xdc.out;                              % Receive element positions
-acq_params.element_dir=[0 0 1];                             % Element directivity
-acq_params.apex=0;                                          % Apex axial position
+acq_params.rx_pos = obj.xdc.out;
+acq_params.tx_pos = [0,0,0];
+acq_params.tx_apod = zeros(obj.xdc.n,1);
+acq_params.tx_apod(obj.xdc.on_elements) = 1;
+acq_params.focus = [obj.xdc.focus(1) 0 obj.xdc.focus(2)];
+if strcmp(obj.xdc.type,'curvilinear')
+    acq_params.apex = obj.xdc.r;
+    acq_params.theta = atand(obj.xdc.focus(1)/(obj.xdc.focus(2)+obj.xdc.r));
+else
+    acq_params.apex=0;
+    acq_params.theta = 0;
+end
+acq_params.steer = [sind(acq_params.theta) 0 cosd(acq_params.theta)];
+[~,n] = max(obj.xdc.icmat(1:obj.grid_vars.nY,:)~=0,[],1);
+acq_params.t0_var = min(n(n~=1));
 
-acq_params.tx_pos=[0,0,0];                                  % Transmit position fixed at origin
-acq_params.theta = 0;                                       % No theta
-acq_params.steer=[sind(acq_params.theta) 0 cosd(acq_params.theta)];
-acq_params.focus=[obj.xdc.focus(1) 0 obj.xdc.focus(2)];
-ind1=find(obj.xdc.delays==0,1,'first');
-foc_pm=sign(obj.xdc.focus(2));
-acq_params.t0_var=foc_pm*(sqrt(sum((acq_params.focus-acq_params.rx_pos(ind1,:)).^2))-...
-    sqrt(sum((acq_params.focus).^2)))/obj.input_vars.c0/obj.grid_vars.dT;
 end
